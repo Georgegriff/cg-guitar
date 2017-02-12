d09a539c1ccd73e0660215ec6d5b22d42f622160
George Griffiths <ggriffiths@idbs.com>
2017-02-12 15:22:59 +0000

    layout

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.use(express.static('' + __dirname + '/dist'));
+app.use(express.static('' + __dirname + '/build/bundled'));
 app.listen(process.env.PORT || 8080);
